5dcbb310ecc4afdf8139f390914902375150e012
Dave Mattice <david.mattice@hotmail.com>
2024-01-21 09:33:55 -0500

    Cleanup outouts

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,33 @@
+
+locals {
+    vms = { for vm in module.virtual_machines : vm.vm.vm_id => {
+            "desc" = vm.vm.description,
+            "name" = format("%s.%s", vm.vm.name, vm.vm.initialization[0].dns[0].domain),
+            "addr" = vm.vm.ipv4_addresses[1][0]
+        }
+    }
+}
+output "virtual_machines" {
+    description = "Details for Virtual Machines created"
+    value = local.vms
+}
+
 #output "data_stores" {
 #    value = data.proxmox_virtual_environment_datastores.first_node
 #}
 
-output "template" {
-    value = data.proxmox_virtual_environment_vms.template
-}
+#output "template" {
+#    value = data.proxmox_virtual_environment_vms.template
+#}
 #output "vms" {
 #    value = format("Tempalte being use is: %s", local.template_name)
 #}
 
-output "virtual_machines" {
-    value = [ 
-        for vm in module.virtual_machines : vm.vm
-    ]
-}
+#output "virtual_machines_all" {
+#    value = [ 
+#        for vm in module.virtual_machines : vm.vm
+#    ]
+#}
 
 #output "user_data" {
 #    description = "File used for user_data"
